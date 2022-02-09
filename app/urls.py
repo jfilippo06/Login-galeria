35fa007fc59695a1aa0b568193fee1d880cc0577
Jfilippo06 <jfilippo04@gmail.com>
2022-02-09 18:36:29 +0100

    admin-store

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
  path('', views.index, name='index'),
- path('store/', views.admin, name='store'),
- path('accounts/login/',auth_views.LoginView.as_view(template_name='app/login.html',next_page='store'))
+ path('admin-store/', views.admin, name='admin-store'),
+ path('accounts/login/',auth_views.LoginView.as_view(template_name='app/login.html',next_page='admin-store')),
+ path('accounts/logout/',auth_views.LogoutView.as_view(template_name='app/index.html',next_page='index')),
 ]
