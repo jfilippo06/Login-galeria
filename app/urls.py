f6b8a9b89b5f02b217708f723556f4a4268caa6b
Jfilippo06 <jfilippo04@gmail.com>
2022-02-10 17:30:12 +0100

    @login_required

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
  path('', views.index, name='index'),
  path('admin-galery/', views.admin, name='admin-galery'),
+ path('admin-galery/editar/<str:id>', views.editar, name='editar'),
+ path('admin-galery/eliminar/<str:id>', views.eliminar, name='eliminar'),
  path('accounts/login/',auth_views.LoginView.as_view(template_name='app/login.html',next_page='admin-galery')),
  path('accounts/logout/',auth_views.LogoutView.as_view(template_name='app/index.html',next_page='index')),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,3 @@
-from multiprocessing import context
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .forms import Send
@@ -19,5 +18,26 @@ def admin(request):
             return redirect('admin-galery')
     else:
         form = Send()
-    context = {'form':form}
+        data = Post.objects.all()
+    context = {'form':form,'data':data}
     return render(request,'app/admin.html',context)
+
+@login_required
+def editar(request,id):
+    id_imagen = Post.objects.get(id=id)
+    if request.method == 'POST':
+        form = Send(request.POST,request.FILES, instance=id_imagen)
+        if form.is_valid():
+            form.save()
+            return redirect('admin-galery')
+    else:
+        form = Send(instance=id_imagen)
+        data = Post.objects.all()
+    context = {'form':form,'data':data}
+    return render(request,'app/admin.html',context)
+
+@login_required
+def eliminar(requet,id):
+    id_imagen = Post.objects.get(id=id)
+    id_imagen.delete()
+    return redirect('admin-galery')
